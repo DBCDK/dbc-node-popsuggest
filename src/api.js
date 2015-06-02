556d9eb61b04ed04ec610c985a347eab4877252d
Martin Møller <mmj@dbc.dk>
2015-06-02 22:17:08 +0200

    started implementing dispatcher into api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,17 @@
 'use strict';
 
+import OpenSuggest from '';
+import Dispatcher from './dispatcher.js';
+
+
 let endPoint = '';
+let dispatcher = null;
+
+function setUpSocket(socket) {
+  dispatcher = Dispatcher();
+  dispatcher.init(socket);
+  dispatcher.listen('getSuggestions', OpenSuggest.getSuggestions); //TODO implement method for retreiving suggestions
+}
 
 export function init(endpoint = null, socket = null) {
   if (!endpoint) {
@@ -10,7 +21,6 @@ export function init(endpoint = null, socket = null) {
 
   if (socket) {
     console.log('Setting up socket');
-    // TODO set up socket
-
+    setUpSocket(socket);
   }
 }
