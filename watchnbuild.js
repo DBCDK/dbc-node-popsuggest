3d4d061d2262677f16aa65217790407c5101e3a8
Martin Møller <mmj@dbc.dk>
2015-06-02 22:16:08 +0200

    improved build watcher and npm scripts

--- watchnbuild.js
+++ watchnbuild.js
@@ -3,9 +3,8 @@
 var watch = require('node-watch');
 var exec = require('child_process').exec;
 
-watch('./src/', {recursive: true, followSymLinks: true}, function(filename) {
-  //console.log(filename, ' changed.');
-  exec("rm -rf ./dist && broccoli build ./dist", function() {
-    console.log(arguments);
+watch('./src/', {recursive: true, followSymLinks: true}, function() {
+  exec("npm run build", function() {
+    console.log(arguments); //TODO handke actual arguments to provide better info rather than just dumping it all
   });
 });
