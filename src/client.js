f21cc71a22b4f719b57ea02595f2491c520aa5d2
Martin Møller <mmj@dbc.dk>
2015-06-05 14:57:35 +0200

    implemented new suggest method

--- src/client.js
+++ src/client.js
@@ -1,18 +1,56 @@
 'use strict';
 
-export function test() {
-  console.log('hest');
+var Promise = require('es6-promise').Promise;
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+let endpoint = process.env.POPSUGGEST_ENDPOINT || null; // eslint-disable-line no-process-env
+
+/**
+ * Retrieves data from the webservice based on the parameters given
+ *
+ * @param {Object} params Parameters for the request
+ * @return {Promise}
+ */
+function sendRequest(params) {
+  return new Promise((resolve, reject) => {
+    client.get(endpoint + '${method}?query=${index}&fields=${fields}', params, (data, response) => {
+      resolve(data);
+    });
+  });
 }
 
+/**
+ * Setting the necessary paramerters for the client to be usable.
+ * The endpoint is only set if endpoint is null to allow setting it through
+ * environment variables.
+ *
+ * @param {Object} config Config object with the necessary parameters to use
+ * the webservice
+ */
 export function init(config) {
   console.log('init: ', config);
+  if (!endpoint) {
+    endpoint = config.endpoint;
+  }
 }
 
+/**
+ * Constructs the objects of parameters for this type of request.
+ *
+ * @param {Object} query Parameters for the request
+ * @return {Promise}
+ */
 export function getSuggestions(query = {}) {
-  console.log(' ');
-  console.log('getSuggestions');
-  console.log('query: ', query);
-  console.log(' ');
+  const params = {
+    path: {
+      method: 'suggest'
+    },
+    parameters: {
+      index: query.index,
+      fields: query.fields.toString()
+    }
+  };
 
-  return {response: 'testhest'};
+  return sendRequest(params);
 }
